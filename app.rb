c78bb8022388eb0dd36d96ddfd64634b477b6f13
silent <silent161192@gmail.com>
2017-08-18 21:04:16 +0300

    bdbarber

--- app.rb
+++ app.rb
@@ -4,15 +4,34 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name=?', [name]).length > 0
+
+end
+
+def seed_db db, barbers
+
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end
+	end	
+end
+
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
 	db.results_as_hash = true
 	return db
 end
 
+before do 
+	db = get_db
+	@barbers = db.execute 'select * from Barbers' 
+end
+
 configure  do
-	@db = SQLite3::Database.new 'barbershop.db'
-	@db.execute 'CREATE TABLE IF NOT EXISTS 
+	db = SQLite3::Database.new 'barbershop.db'
+	db.execute 'CREATE TABLE IF NOT EXISTS 
 	"users" 
 	(
 			"Id"	INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,7 +41,18 @@ configure  do
 			"barber"	TEXT,
 			"color"	TEXT
 	)'
+
+	db.execute 'CREATE TABLE IF NOT EXISTS 
+	"Barbers" 
+	(
+			"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+			"name"	TEXT
+		
+	)'
+
+	seed_db db, ['Jessie', 'Walter', 'Gus', 'Ferrari', 'Loool']
 end
+
 get '/' do
 
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
@@ -33,11 +63,14 @@ get '/about' do
 	erb :about
 end
 
+
 get '/visit' do
 	erb :visit
 end
 
 post '/visit' do
+
+
 
 	@aaa = params[:username]
 	@bbb = params[:phone]
@@ -77,7 +110,7 @@ end
 get '/showusers' do
 	db = get_db
 
-	@results = db.execute 'select* from Users order by id desc'
+	@results = db.execute 'select* from users order by id desc'
 
   erb :showusers
 end
